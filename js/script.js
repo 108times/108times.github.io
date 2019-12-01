4dc00404181a92875696981727b989c2d276acef
Amir <amirkadenov@gmail.com>
2019-12-01 21:52:33 +0600

    Changed about page. Added new text

--- js/script.js
+++ js/script.js
@@ -666,7 +666,8 @@ ready = $(document).ready(function () {
           "<div class='row page-row'>" +
           "<div   class='page-inner text-justify card card-body container-fluid col-md-6 col-sm-12'>" +
           "<h1 class='m-auto p-3'>Hello, I'm Amir.</h1>" +
-          "<p>I'm a web developer, living and studying in Almaty, Kazakhstan. My passion is creating and I find happiness in being a better me today than the me yesterday. Feel free to take a look at my latest projects on the <span class = 'link-highlighted' href='#portfolio' title = 'portfolio'>portfolio page.</span></p></div> </div> </div></section>"
+          "<p>I'm a web developer, living and studying in Almaty, Kazakhstan. My passion is creating and I find happiness in being a better me today than the me yesterday. Feel free to take a look at my skillset on the <a class = 'link-highlighted' href='#skills' title = 'skills'>skills page, </a>" +
+          "check my latest projects on the <a class = 'link-highlighted' href='#portfolio' title = 'portfolio'>portfolio page</a> and contact me on the <a class = 'link-highlighted' href='#contact' title = 'contact'>contact page.</a></p></div> </div> </div></section>"
         );
         break;
 
@@ -923,7 +924,8 @@ ready = $(document).ready(function () {
           "<div class='row page-row'>" +
           "<div   class='page-inner text-justify card card-body container-fluid col-md-6 col-sm-12'>" +
           "<h1 class='m-auto p-3'>Hello, I'm Amir.</h1>" +
-          "<p>I'm a web developer, living and studying in Almaty, Kazakhstan. My passion is creating and I find happiness in being a better me today than the me yesterday. Feel free to take a look at my latest projects on the <span class = 'link-highlighted' href='#portfolio' title = 'portfolio'>portfolio page.</span></p></div> </div> </div></section>"
+          "<p>I'm a web developer, living and studying in Almaty, Kazakhstan. My passion is creating and I find happiness in being a better me today than the me yesterday. Feel free to take a look at my skillset on the <a class = 'link-highlighted' href='#skills' title = 'skills'>skills page, </a>" +
+          "check my latest projects on the <a class = 'link-highlighted' href='#portfolio' title = 'portfolio'>portfolio page</a> and contact me on the <a class = 'link-highlighted' href='#contact' title = 'contact'>contact page.</a></p></div> </div> </div></section>"
         );
         break;
 
